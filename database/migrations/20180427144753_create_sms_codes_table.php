c88a07228157937e80796ffce214fcfa48acd242
lsw <1170394858@qq.com>
2018-04-27 23:03:04 +0800

    增加短信表

---
+++ database/migrations/20180427144753_create_sms_codes_table.php
@@ -0,0 +1,40 @@
+<?php
+
+
+use Phinx\Migration\AbstractMigration;
+
+class CreateSmsCodesTable extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change()
+    {
+        $this->table("sms_codes")
+            ->addColumn("mobile", "string", ["limit" => 11])
+            ->addColumn("code", "string", ["limit" => 10])
+            ->addColumn("template", "string", ["limit" => 10, "comment" => "模板"])
+            ->addColumn("status", "integer", ["limit" => 10, "comment" => "状态 1发送中 2发送成功 3发送失败", "default" => 0])
+            ->addColumn("memo", "string", ["limit" => 30, "comment" => "回调", "null" => true])
+            ->addTimestamps()
+            ->save();
+    }
+}
