3b5957240874d838e46f62c23605542478e84a1b
少文 <lishaowen@dowedo.com.cn>
2018-05-04 17:27:34 +0800

    消息队列发送短信优化

--- app/Jobs/SmsCode.php
+++ app/Jobs/SmsCode.php
@@ -4,11 +4,14 @@ require_once __DIR__ . "/../../bootstrap/bootstrap.php";
 while (true) {
     $job = di("queue")->watch("SmsCode")->reserve();
     $data = unserialize($job->getData());
-    $smsCode = $data['smsCode'];
+    $smsCodeId = $data['smsCodeId'];
     $token = $data['token'];
     //业务逻辑
-    service("sms/manager")->send($smsCode, $token);
+    if (service("sms/manager")->send($smsCodeId, $token)) {
+        di("queue")->delete($job);
+    } else {
+        di("queue")->bury($job, 10240);
+    }
     di("cache")->delete($token . "_tcgmc");
-    di("queue")->delete($job);
 }
 
