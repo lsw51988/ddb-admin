76f9846d438da14a39c7e49f76351a7ece745e70
少文 <lishaowen@dowedo.com.cn>
2018-05-10 15:17:36 +0800

    重新提交

--- app/Controllers/WechatAuthController.php
+++ app/Controllers/WechatAuthController.php
@@ -15,13 +15,14 @@ class WechatAuthController extends BaseController
 {
     protected $token = "";
     protected $currentMember = "";
+
     public function onConstruct()
     {
         parent::onConstruct();
         $token = $this->request->getHeader("token");
-        if($token == ""){
+        if ($token == "") {
             return $this->error("请重新登录");
-        }else{
+        } else {
             $this->token = $token;
             //用户更新信息时,需求重新写入缓存
             $this->currentMember = unserialize(di("cache")->get($token));
