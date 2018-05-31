3ec9d31f7472c0367c5ac00ac842bde58f02e971
少文 <lishaowen@dowedo.com.cn>
2018-05-31 12:26:11 +0800

    支付调试

--- app/Controllers/TestController.php
+++ app/Controllers/TestController.php
@@ -67,4 +67,32 @@ class TestController extends BaseController
     public function testConfigAction(){
         print_r(di("config"));
     }
+
+    /**
+     * @Get("/pay")
+     */
+    public function strAction(){
+        $url = "https://api.mch.weixin.qq.com/pay/unifiedorder";
+        $param = [];
+        $param['appid'] = "wx3bd752036e968963";
+        $param['body'] = "电动帮-积分充值";
+        $param['mch_id'] = "1504528021";
+        $param['nonce_str'] = md5(di("security")->hash('1'.rand(100000,99999).time()));
+        $param['notify_url'] = "https://www.ebikea.com/wechat/point/pay_callback";
+        $param['openid'] = "oKgHM4myAdesr0AyoIU_JXjvOVz8";
+        $param['out_trade_no'] = "2018053101";
+        $param['spbill_create_ip'] = "121.225.203.141";
+        $param['total_fee'] = "8";//分钱
+        $param['trade_type'] = "JSAPI";
+
+        $sign = "";
+        foreach ($param as $k=>$v){
+            $sign = $sign.$k."=".$v."&";
+        }
+        $sign = substr($sign,0,(strlen($sign)-1));
+        $param['sign'] = $sign;
+
+        $result = curl_request($url, "POST", $param);
+        print_r($result);
+    }
 }
--- app/Controllers/Wechat/PointController.php
+++ app/Controllers/Wechat/PointController.php
@@ -48,16 +48,33 @@ class PointController extends WechatAuthController
         $url = "https://api.mch.weixin.qq.com/pay/unifiedorder";
         $param = [];
         $param['appid'] = "wx3bd752036e968963";
+        $param['body'] = "电动帮-积分充值";
         $param['mch_id'] = "1504528021";
         $param['nonce_str'] = md5(di("security")->hash($member->getId().rand(100000,99999).time()));
-        $param['body'] = "积分充值";
-        $param['out_trade_no'] = "20150806125346";
-        $param['total_fee'] = "8";//分钱
+        $param['notify_url'] = "https://www.ebikea.com/wechat/point/pay_callback";
+        $param['openid'] = "oKgHM4myAdesr0AyoIU_JXjvOVz8";
+        $param['out_trade_no'] = "2018053101";
         $param['spbill_create_ip'] = "121.225.203.141";
+        $param['total_fee'] = "8";//分钱
+        $param['trade_type'] = "JSAPI";
 
+        $sign = "";
+        foreach ($param as $k=>$v){
+            $sign = $sign.$k."=".$v."&";
+        }
+        $sign = substr($sign,0,(strlen($sign)-1));
+        $param['sign'] = $sign;
 
+        $result = curl_request($url, "POST", $param);
+        print_r($result);
+    }
 
-        curl_request($url, "POST", $param = []);
+    /**
+     * @Get("/pay_callback")
+     */
+    public function payCallbackAction(){
+        $data = $this->data;
+        app_log("pay")->info($data);
     }
 
 }
